7e39986dc0eb8169a99585c9f476d5c46fd93f8c
HanSu Lee <i.hansu.lee@gmail.com>
2023-05-09 14:10:41 +0900

    feat(components): add `Typo` component

---
+++ src/components/common/Typo/styled.ts
@@ -0,0 +1,18 @@
+import styled from '@emotion/styled';
+
+export const H1 = styled.h1`
+  margin: 0;
+  font-size: 2.4rem;
+`;
+
+export const H2 = styled.h2`
+  margin: 0;
+  font-size: 2rem;
+`;
+
+export const Small = styled.p`
+  margin: 0;
+  color: #b6b6b6;
+  font-size: 1.4rem;
+  font-weight: 400;
+`;
--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,3 +1,4 @@
 export * from './GameSchedule';
 export * from './Navbar';
 export * from './Tag';
+export * from './Typo';
